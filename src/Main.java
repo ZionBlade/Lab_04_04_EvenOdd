86a9978918f87651dc23cc9b14ffc97e368ade71
ZionBlade <wernerct@mail.uc.edu>
2022-09-16 19:51:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int testNum = 2;
+        if (testNum % 2 == 0) {
+            System.out.println(testNum + " is even.");
+        } else {
+            System.out.println(testNum + " is odd");
+        }
+    }
+}
